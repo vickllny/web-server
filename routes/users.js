08b8aee300bf9c1fac1773679e649afa474be324
vickllny <vickllny@163.com>
2024-02-28 23:07:04 +0800

    本地调试开发使用https-proxy-agent

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,26 @@
 var express = require('express');
+
+//本地开发使用
+const { HttpsProxyAgent } = require('https-proxy-agent')
+const httpAgent = new HttpsProxyAgent('http://127.0.0.1:7890')
+
+var OpenAI = require('openai');
+const openai = new OpenAI({
+  apiKey: "",
+  httpAgent
+});
+
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  const completion = openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
+  });
+  
+  console.log(completion.choices[0]);
   res.send('respond with a resource');
 });
 
