6020c117c719f6fcc1e5822ea1a065dc4d6b4b7f
vickllny <vickllny@163.com>
2024-02-28 23:20:50 +0800

    修改服务端端口为8000

--- app.js
+++ app.js
@@ -38,4 +38,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+process.env.PORT = 8000;
+
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -14,14 +14,14 @@ const openai = new OpenAI({
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  const completion = openai.chat.completions.create({
+router.get('/', async function(req, res, next) {
+  const completion = await openai.chat.completions.create({
     messages: [{ role: "system", content: "You are a helpful assistant." }],
     model: "gpt-3.5-turbo",
   });
   
   console.log(completion.choices[0]);
-  res.send('respond with a resource');
+  res.send(completion.choices[0]);
 });
 
 module.exports = router;
